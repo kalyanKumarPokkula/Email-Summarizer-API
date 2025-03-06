5cdf53b17b5a08f7aeabef62337399843f14b9b7
kalyan <kalyankumarpokkula@gmail.com>
2025-03-06 17:41:02 +0530

    added some changes in a code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from llm import email_summarizer
+from llm import email_summarizer, email_reply
 from fastapi import FastAPI , HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import json
+import re
 
 app = FastAPI()
 
@@ -26,10 +27,39 @@ def read_root():
 @app.post("/summarize")
 def summarize(body: dict):
     email = body["email"]
-    # data = email_summarizer(email)
-    # print(data)
-    cleaned_json_data = email_summarizer(email).strip('```json\n').strip('```')
-    print(cleaned_json_data)
-    data = json.loads(cleaned_json_data)
-    return data
+    privacy_mode = body["privacy_mode"]
+    if not email:
+        raise HTTPException(status_code=400, detail="Email field is required")
+    cleaned_json_data = email_summarizer(email, privacy_mode)
 
+    # Use regex to extract valid JSON content
+    match = re.search(r'```json\n(.*)```', cleaned_json_data, re.DOTALL)
+    
+    if match:
+        cleaned_json_data = match.group(1)  # Extract JSON part
+
+    # Convert JSON string to dictionary
+    try:
+        data = json.loads(cleaned_json_data)
+        return data
+    except json.JSONDecodeError as e:
+        print("Error decoding JSON:", e)
+        return None
+
+@app.post("/reply")
+def reply(body : dict):
+    email = body["email"]
+    privacy_mode = body["privacy_mode"]
+
+    response = email_reply(email, privacy_mode)
+    print(response)
+    return response
+
+
+
+# json_Data = "```json\n{\n  \"summary\": \"Chaitanya Gadhe has a Backend Developer contract role opportunity and is seeking Kalyan Kumar's interest and availability for a discussion.\",\n  \"action_items\": [\n    \"Send Chaitanya Gadhe your CV\",\n    \"Respond with a good time for a quick chat\"\n  ]\n}\n```"
+# cleaned_json_data = json_Data.strip('```json\n').strip('```')
+# print(type(cleaned_json_data))
+# data = json.loads(cleaned_json_data)
+# print(type(data))
+# print(data)
